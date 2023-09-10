db5c1892451ecb820dc63d813a9d5e8ee8c7c350
EJIeKTpa <sawwaksu@yandex.ru>
2023-09-10 23:42:31 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] randomArray = generateRandomArray(4);
+        System.out.println("Исходный массив: " + Arrays.toString(randomArray));
+
+        int[] sortedArray = sortArray(randomArray);
+        System.out.println("Упорядоченный массив: " + Arrays.toString(sortedArray));
+    }
+
+    private static int[] generateRandomArray(int size) {
+        int[] array = new int[size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt();
+        }
+
+        return array;
+    }
+
+    private static int[] sortArray(int[] array) {
+        int[] sortedArray = array.clone();
+        int temp;
+
+        for (int i = 0; i < sortedArray.length - 1; i++) {
+            for (int j = 0; j < sortedArray.length - i - 1; j++) {
+                if (sortedArray[j] > sortedArray[j + 1]) {
+                    temp = sortedArray[j];
+                    sortedArray[j] = sortedArray[j + 1];
+                    sortedArray[j + 1] = temp;
+                }
+            }
+        }
+
+        return sortedArray;
     }
 }
